fbeba32bdaebdb105281761ebd649c832c71fb4b
Alaeddine <alaeddinefattouch@insat.u-carthage.tn>
2023-05-08 19:43:37 +0100

    first commit

---
+++ src/test/TestEquationSecondDegre.java
@@ -0,0 +1,34 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import jmf.equation.*;
+
+public class TestEquationSecondDegre {
+
+    @Test
+    public void test2Racines() {
+        PolynomeSecondDegre p = new PolynomeSecondDegre(1, 1, -2);
+        double[] racines = p.calculRacines();
+        assertEquals(2, racines.length);
+        assertEquals(1, racines[0], 0.0001);
+        assertEquals(-2, racines[1], 0.0001);
+    }
+
+    @Test
+    public void test1Racine() {
+        PolynomeSecondDegre p = new PolynomeSecondDegre(1, -2, 1);
+        double[] racines = p.calculRacines();
+        assertEquals(1, racines.length);
+        assertEquals(1, racines[0], 0.0001);
+    }
+
+    @Test
+    public void testPasDeRacineReelle() {
+        PolynomeSecondDegre p = new PolynomeSecondDegre(1, 1, 1);
+        double[] racines = p.calculRacines();
+        assertNull(racines);
+    }
+
+}
